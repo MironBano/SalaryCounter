bba66eb0938280e4838eb6b3889e2c977e44c0c1
Miron <rybak.m.yu@gmail.com>
2024-09-20 14:35:33 +0300

    ViewBinding added BackPressedCallback handled coroutineScope changed to lifecycleScope Strings extracted to resources

--- app/src/main/java/com/bano/mynewproject/CounterActivity.kt
+++ app/src/main/java/com/bano/mynewproject/CounterActivity.kt
@@ -3,17 +3,17 @@ package com.bano.mynewproject
 import android.annotation.SuppressLint
 import android.content.Intent
 import android.os.Bundle
-import android.widget.Button
-import android.widget.TextView
 import android.widget.Toast
 import androidx.appcompat.app.AppCompatActivity
 import kotlinx.coroutines.*
+import androidx.activity.addCallback
+import androidx.lifecycle.lifecycleScope
+import com.bano.mynewproject.databinding.ActivityCounterBinding
 
 
 class CounterActivity : AppCompatActivity() {
 
-    private lateinit var counterTV: TextView
-    private lateinit var stopBTN: Button
+    private lateinit var binding: ActivityCounterBinding
     private lateinit var counterJob: Job
     private var counter = 0.0
     private var lastBackPressedTime: Long = 0
@@ -21,62 +21,55 @@ class CounterActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_counter)
-
-        counterTV = findViewById(R.id.counterTV)
-        stopBTN = findViewById(R.id.stopBTN)
+        binding = ActivityCounterBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+        setupBackPressedCallback()
 
         val salaryInformation: SalaryInformation? = intent.getParcelableExtra("salaryInformation")
 
-        counterTV.text = salaryInformation!!.amount.toString()
+        binding.counterTV.text = salaryInformation!!.amount.toString()
+        binding.stopBTN.setOnClickListener {
+            startActivity(
+                Intent(
+                    this, MainActivity::class.java
+                ).setFlags(Intent.FLAG_ACTIVITY_NEW_TASK or Intent.FLAG_ACTIVITY_CLEAR_TASK)
+            )
+        }
 
         counterJob = CoroutineScope(Dispatchers.Main).launch { startCounter(salaryInformation) }
 
-        stopBTN.setOnClickListener {
-            try {
-                if (counterJob.isActive) counterJob.cancel()
-            } catch (_: UninitializedPropertyAccessException) {
-            }
-
-            startActivity(
-                Intent(this, MainActivity::class.java)
-                    .setFlags(Intent.FLAG_ACTIVITY_NEW_TASK or Intent.FLAG_ACTIVITY_CLEAR_TASK)
-            )
-        }
     }
 
+    @SuppressLint("DefaultLocale", "SetTextI18n")
     private suspend fun startCounter(salaryInfo: SalaryInformation) {
         val amount = salaryInfo.amount.toDouble()
         val hours = salaryInfo.hoursInDay.toDouble()
         val days = salaryInfo.days.toDouble()
         val amountUnit: Double = amount / days / hours / 3600
-        coroutineScope {
-            launch {
-                while (true) {
-                    counter += amountUnit
-                    counterTV.text = String.format("%.2f", counter)
-                    delay(1000)
-                }
+        lifecycleScope.launch {
+            while (true) {
+                counter += amountUnit
+                binding.counterTV.text = String.format("%.2f", counter) + "₽"
+                delay(1000)
             }
         }
     }
 
-    @SuppressLint("MissingSuperCall")
-    override fun onBackPressed() {
-        if (System.currentTimeMillis() - lastBackPressedTime < exitDelay) {
-            try {
-                if (counterJob.isActive) counterJob.cancel()
-            } catch (_: UninitializedPropertyAccessException) {
+    private fun setupBackPressedCallback() {
+        onBackPressedDispatcher.addCallback(this) {
+            if (System.currentTimeMillis() - lastBackPressedTime < exitDelay) {
+                startActivity(
+                    Intent(
+                        this@CounterActivity, MainActivity::class.java
+                    ).setFlags(Intent.FLAG_ACTIVITY_NEW_TASK or Intent.FLAG_ACTIVITY_CLEAR_TASK)
+                )
+            } else {
+                lastBackPressedTime = System.currentTimeMillis()
+                Toast.makeText(
+                    this@CounterActivity, R.string.warningClosingActivity, Toast.LENGTH_SHORT
+                ).show()
             }
-            startActivity(
-                Intent(this, MainActivity::class.java)
-                    .setFlags(Intent.FLAG_ACTIVITY_NEW_TASK or Intent.FLAG_ACTIVITY_CLEAR_TASK)
-            )
-        } else {
-            lastBackPressedTime = System.currentTimeMillis()
-            Toast.makeText(this, "Нажмите ещё раз, чтобы выйти", Toast.LENGTH_SHORT).show()
         }
     }
-
 
 }
--- app/src/main/java/com/bano/mynewproject/MainActivity.kt
+++ app/src/main/java/com/bano/mynewproject/MainActivity.kt
@@ -3,12 +3,10 @@ package com.bano.mynewproject
 import android.content.Intent
 import android.os.Bundle
 import android.view.View
-import android.widget.Button
-import android.widget.EditText
 import android.widget.Toast
+import androidx.activity.addCallback
 import androidx.appcompat.app.AppCompatActivity
 import com.bano.mynewproject.databinding.ActivityMainBinding
-import kotlinx.coroutines.channels.*
 
 class MainActivity : AppCompatActivity() {
 
@@ -20,34 +18,36 @@ class MainActivity : AppCompatActivity() {
         super.onCreate(savedInstanceState)
         binding = ActivityMainBinding.inflate(layoutInflater)
         setContentView(binding.root)
-
+        setupBackPressedCallback()
         binding.button.setOnClickListener(buttonClickListener())
     }
 
     private fun buttonClickListener() = View.OnClickListener {
-        if (binding.editTextNumber.text.isNotEmpty() &&
-            binding.editTextNumber2.text.isNotEmpty() &&
-            binding.editTextNumber3.text.isNotEmpty()) {
-
+        if (binding.editTextNumber.text.isNotEmpty() && binding.editTextNumber2.text.isNotEmpty() && binding.editTextNumber3.text.isNotEmpty()) {
             val amount = binding.editTextNumber.text.toString().toInt()
             val hoursInDay = binding.editTextNumber2.text.toString().toInt()
             val totalDays = binding.editTextNumber3.text.toString().toInt()
             val salaryInformation = SalaryInformation(amount, hoursInDay, totalDays)
 
-            val intent = Intent(this, CounterActivity::class.java)
+            val intent = Intent(this@MainActivity, CounterActivity::class.java)
             intent.putExtra("salaryInformation", salaryInformation)
             startActivity(intent)
         } else {
-            Toast.makeText(this, "Пожалуйста, заполните все поля", Toast.LENGTH_SHORT).show()
+            Toast.makeText(this, R.string.warningMissingInformation, Toast.LENGTH_SHORT).show()
         }
     }
 
-    override fun onBackPressed() {
-        if (System.currentTimeMillis() - lastBackPressedTime < exitDelay) {
-            super.onBackPressed()
-        } else {
-            lastBackPressedTime = System.currentTimeMillis()
-            Toast.makeText(this, "Нажмите ещё раз, чтобы выйти", Toast.LENGTH_SHORT).show()
+    private fun setupBackPressedCallback() {
+        onBackPressedDispatcher.addCallback(this) {
+            if (System.currentTimeMillis() - lastBackPressedTime < exitDelay) {
+                finish()
+            } else {
+                lastBackPressedTime = System.currentTimeMillis()
+                Toast.makeText(
+                    this@MainActivity, R.string.warningClosingActivity, Toast.LENGTH_SHORT
+                ).show()
+            }
         }
     }
+
 }
